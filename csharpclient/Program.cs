47a9a5ae89a4ac732b16749d399bf5cdea06b539
Michael Eselgroth <meselgroth@yahoo.com>
2020-04-11 16:33:54 +1000

    Client: Start receiver on new task. Send message from received input.

--- csharpclient/Program.cs
+++ csharpclient/Program.cs
@@ -8,13 +8,32 @@ namespace csharpclient
 {
     class Program
     {
+        private static ClientWebSocket webSocket;
+
         static async Task Main(string[] args)
         {
-            var webSocket = new ClientWebSocket();
+            webSocket = new ClientWebSocket();
             await webSocket.ConnectAsync(new Uri("ws://localhost:5000/"), CancellationToken.None);
 
+            var receiverTask = Task.Run(async () => await Receiver());
+            var input = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter message to send (x to close):");
+                input = Console.ReadLine();
+
+                await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(input)), WebSocketMessageType.Text, true, CancellationToken.None);
+            } while (input != "x");
+
+            // Note currently doesn't cancel Receiver and throws error
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "ok", CancellationToken.None);
+        }
+
+        private static async Task Receiver()
+        {
             var buffer = new byte[1024 * 4];
             WebSocketReceiveResult receivedMsg;
+
             do
             {
                 receivedMsg = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
